cd7f8cf8b8f931749d22eaac4469acdbd00cd611
C-rostad <Carter@rostad.ca>
2025-06-26 10:47:33 -0700

    Added registration page and get method

--- express_server.js
+++ express_server.js
@@ -47,6 +47,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  res.render("register");
+})
+
 app.get("/urls/new", (req, res) => {
   const templateVars = {
     username: req.cookies["username"]
